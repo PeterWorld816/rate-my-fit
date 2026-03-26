59d8ea90643f0c8fe1935d9719023f617d96cf1d
Peter Kim <petersangkyukim@gmail.com>
2026-03-26 00:11:46 -0400

    add upload + rate feature with CORS

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
-const express = require('express');
-const app = express();
+import express from 'express';
+import cors from 'cors';
 
+const app = express();
 const PORT = 5000;
+
+// ⭐⭐⭐ 이거 꼭 있어야 함
+app.use(cors());
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('🔥 Backend running!');
+});
+
+app.post('/rate', (req, res) => {
+  const randomScore = Math.floor(Math.random() * 101);
+
+  res.json({
+    score: randomScore
+  });
 });
 
 app.listen(PORT, () => {
